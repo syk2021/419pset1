fbcc7797e930a68ff6823b773332c6c14af33cf2
phucd5 <phucduongh@gmail.com>
2023-02-12 13:06:54 -0500

    project start

--- lux.py
+++ lux.py
@@ -3,19 +3,9 @@ from contextlib import closing
 from sqlite3 import connect
 from table import Table
 
-with connect('./lux.sqlite', isolation_level=None, uri=True) as connection:
-    with closing(connection.cursor()) as cursor:
-        smt_str = "SELECT * FROM objects LIMIT 5"
-        cursor.execute(smt_str)
-        data = cursor.fetchall()
-        print(len(data))
+class LuxCLI():
+    def __init__(self) -> None:
+        pass
 
-        rows_list = []
-        
-        for row in data:
-            rows_list.append(list(row))
-
-        print(Table(["ID", "label", "name"], [["hi", "hi", "hi"]]))
-
-        
-        
+if __name__ == '__main__':
+    print("Hello")
